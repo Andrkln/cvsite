a2647a35f92806db651b5058ec8f14d2a6661172
Andrkln <andrew13338@gmail.com>
2024-03-28 22:54:52 +0200

    gggg

--- src/app/components/self.js
+++ src/app/components/self.js
@@ -11,56 +11,11 @@ import Image from 'next/image';
 
 
 const cornerAnimation = keyframes`
-    0% {
-        border-top-left-radius: 1% 50%;
-    }
-    12.5% {
-        border-top-left-radius: 50%;
-        border-top-right-radius: 50%;
-    }
-    25% {
-        border-top-right-radius: 1% 50%;
-    }
-    25.001% {
-        border-top-right-radius: 50% 1%;
-    }
-    37.5% {
-        border-top-right-radius: 50%;
-        border-bottom-right-radius: 50%;
-    }
-    50% {
-        border-bottom-right-radius: 50% 1%;
-    }
-    50.001% {
-        border-bottom-right-radius: 1% 50%;
-    }
-    62.5% {
-        border-bottom-right-radius: 50%;
-        border-bottom-left-radius: 50%;
-    }
-    75% {
-        border-bottom-left-radius: 1% 50%;
-    }
-    75.001% {
-        border-bottom-left-radius: 50% 1%;
-    }
-    87.5% {
-        border-bottom-left-radius: 50%;
-        border-top-left-radius: 50%;
-    }
-    100% {
-        border-top-left-radius: 50% 1%;
-    }
+
 `;
 
 const changeAnimation = keyframes`
-    0%, 2.44% {
-        height: 99%;
-    }
 
-    2.45%, 100% {
-        height: 100%;
-    }
 `
 
 const selfCss = css`
