258881be6915250db634bc4048e4962655bced1a
Andrkln <andrew13338@gmail.com>
2024-05-24 08:18:17 +0300

    fgfgfg

--- src/app/hooks/useChat.js
+++ src/app/hooks/useChat.js
@@ -34,6 +34,7 @@ const useChating = () => {
                 
                 const preChunkStr = decoder.decode(value);
                 const preChunks = preChunkStr.split('\n');
+                console.log(preChunks)
                 let sentence = ''
                 
                 for (let preChunk of preChunks) {
